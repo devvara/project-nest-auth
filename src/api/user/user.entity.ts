c46d3e6f57a2ff8a1499ac0b9370adfc32bb542f
Yoochan Hwang <yoochan1868@gmail.com>
2023-07-22 21:25:57 +0900

    Modified user entity

--- src/api/user/user.entity.ts
+++ src/api/user/user.entity.ts
@@ -1,27 +1,33 @@
 import { Exclude } from 'class-transformer';
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   public id: number;
 
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ name: 'email', type: 'varchar', length: 100, nullable: false })
   public email: string;
 
   @Exclude()
-  @Column({ type: 'varchar', nullable: false })
+  @Column({ name: 'password', type: 'varchar', length: 100, nullable: false })
   public password: string;
 
-  @Column({ type: 'varchar', nullable: false })
-  public mbti: string;;
-
-  @Column({ type: 'varchar', nullable: true })
+  @Column({ name: 'nickname', type: 'varchar', length: 50, nullable: true })
   public name: string | null;
 
-  @Column({ type: 'varchar', nullable: true })
+  @Column({ type: 'varchar', length: 10, nullable: false })
+  public mbti: string;
+
+  @Column({ type: 'date', nullable: true })
   public birthdate: string | null;
 
-  @Column({ type: 'varchar', nullable: true, default: null })
-  public lastLoginAt: Date | null;
+  @CreateDateColumn({ name: 'last_login_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+  lastLoginAt: Date;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)', onUpdate: 'CURRENT_TIMESTAMP(6)' })
+  updatedAt: Date;
 }
