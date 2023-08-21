97d1a0d7c92fbf9936331f55b12939147c1c7f23
Yoochan Hwang <yoochan1868@gmail.com>
2023-08-21 23:28:36 +0900

    Add google strategy

---
+++ src/api/user/auth/strategies/google.strategy.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { Profile, Strategy, VerifyCallback } from "passport-google-oauth20";
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly config: ConfigService) {
+    super({
+      clientID: config.get('GOOGLE_CLIENT_ID'),
+      clientSecret: config.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: config.get('GOOGLE_CALLBACK_URL'),
+      scope: ['email', 'profile']
+    });
+  }
+
+  authorizationParams(): { [key: string]: string } {
+    return {
+      access_type: 'offline',
+      prompt: 'consent',
+    }
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback): Promise<any> {
+    try {
+      const { name, emails, photos } = profile;
+      const user = {
+        email: emails[0].value,
+        firstName: name.givenName,
+        lastName: name.familyName,
+        picture: photos[0].value,
+        accessToken
+      }
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+
+}
